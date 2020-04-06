b6a04088af905b1b5c4f3bc259a549f166636036
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-06 15:16:24 -0500

    Format the files

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@
  * @param {String} idName (Optional) The ID tag of the element.
  * @param {String} elementType (Optional) The type of the element it will be created. <div> by default.
  */
-export default function ezCreate(appendTo: HTMLElement | null, className: string = '', idName: string = '', elementType: string = 'div'): HTMLElement {
+export default function ezCreate(
+  appendTo: HTMLElement | null,
+  className: string = '',
+  idName: string = '',
+  elementType: string = 'div',
+): HTMLElement {
   const element = document.createElement(elementType);
   if (className !== '') element.className = className;
   if (idName !== '') element.id = idName;
