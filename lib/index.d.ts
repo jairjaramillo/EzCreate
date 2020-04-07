31bece03a0f60822b060a87caecb8d234cf8cb5d
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-07 01:15:36 -0500

    initial Babel implementation

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Creates an HTML element and appends it to another HTML element.
+ * @param {HTMLElement | null} appendTo HTML element where the create function witll append to. Use NULL to avoid this.
+ * @param {String} className (Optional) The class or classes the element will have.
+ * @param {String} idName (Optional) The ID tag of the element.
+ * @param {String} elementType (Optional) The type of the element it will be created. <div> by default.
+ */
+export default function ezCreate(appendTo: HTMLElement | null, className?: string, idName?: string, elementType?: string): HTMLElement;
+//# sourceMappingURL=index.d.ts.map
