718d1af63956c3ea58d7d480a05118457ae72b43
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-08-16 17:06:27 -0500

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @param {HTMLElement | null} appendTo HTML element where the create function witll append to. Use NULL to avoid this.
  * @param {String} className (Optional) The class or classes the element will have.
  * @param {String} idName (Optional) The ID tag of the element.
- * @param {String} elementType (Optional) The type of the element it will be created. <div> by default.
+ * @param {String} elementType (Optional) The type of the element it will be created. <div> is used by default.
  */
 export default function ezCreate(
   appendTo: HTMLElement | null,
